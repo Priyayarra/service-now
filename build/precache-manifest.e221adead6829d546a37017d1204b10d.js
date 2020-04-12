316f6868daabb9297a2dd67d07487e6ae88b8c03
priyayarra <priyanka@signalx.ai>
2020-04-12 10:31:08 +0530

    yarn build changes

---
+++ build/precache-manifest.e221adead6829d546a37017d1204b10d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa8002df7105c0cba9847b041416f7ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1413e4724c764ae4ac98",
+    "url": "/static/css/main.ae32c07f.chunk.css"
+  },
+  {
+    "revision": "805cfec9c8ac0d5b3dc9",
+    "url": "/static/js/2.544a442b.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.544a442b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1413e4724c764ae4ac98",
+    "url": "/static/js/main.abe93445.chunk.js"
+  },
+  {
+    "revision": "a9dd95faaca53876dbc8",
+    "url": "/static/js/runtime-main.b9faac23.js"
+  }
+]);
